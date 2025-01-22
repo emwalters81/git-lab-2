30156ac1071125d5e789499b4d38454ef5128117
emwalters81 <emwalters81@outlook.com>
2025-01-21 20:10:10 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Evan Walters
+ *        Date: 1/21/2025
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
